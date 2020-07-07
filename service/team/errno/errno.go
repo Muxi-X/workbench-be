097205503a7e89698b3551b2ffe947995f12915c
MitsuhaOma <2391542095@qq.com>
2020-07-07 13:47:25 +0800

    :tada: new file

--- service/team/errno/errno.go
+++ service/team/errno/errno.go
@@ -6,7 +6,11 @@ import (
 
 var (
 	OK  = &err.Errno{Code: 0, Message: "OK"}
+
 	ErrDatabase = &err.Errno{Code: 10001, Message: "Database error"}
+
+    //group
+	ErrPermissionDenied = &err.Errno{Code: 20101, Message: "Permission Denied"}
 	)
 
 
