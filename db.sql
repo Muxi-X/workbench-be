b106d2697740d47ce6ff79405865f45db9a1d638
Shadowmaple <1142319190@qq.com>
2020-07-30 14:15:39 +0800

    [feat]: update db.sql

--- db.sql
+++ db.sql
@@ -89,17 +89,19 @@ CREATE TABLE `user2projects` (
 -- Table structure for applys
 -- ----------------------------
 
+DROP TABLE IF EXISTS `applys`;
 CREATE TABLE `applys` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for groups
 -- ----------------------------
 
+DROP TABLE IF EXISTS `groups`;
 CREATE TABLE `groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(10) DEFAULT NULL,
@@ -116,6 +118,7 @@ CREATE TABLE `groups` (
 -- Table structure for teams
 -- ----------------------------
 
+DROP TABLE IF EXISTS `teams`;
 CREATE TABLE `teams` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(10) DEFAULT NULL,
@@ -125,3 +128,105 @@ CREATE TABLE `teams` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+
+-- ----------------------------
+-- Table structure for comments
+-- ----------------------------
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kind` int(11) DEFAULT NULL,
+  `content` text,
+  `time` varchar(50) DEFAULT NULL,
+  `creator` int(11) DEFAULT NULL,
+  `doc_id` int(11) DEFAULT NULL,
+  `file_id` int(11) DEFAULT NULL,
+  `statu_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `doc_id` (`doc_id`),
+  KEY `file_id` (`file_id`),
+  KEY `statu_id` (`statu_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+
+-- --------------------------------------------
+-- Table structure for docs, files and folders
+-- --------------------------------------------
+
+DROP TABLE IF EXISTS `docs`;
+CREATE TABLE `docs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(150) DEFAULT NULL,
+  `content` text,
+  `re` tinyint(1) DEFAULT NULL,
+  `top` tinyint(1) DEFAULT NULL,
+  `create_time` varchar(30) DEFAULT NULL,
+  `delete_time` varchar(30) DEFAULT NULL,
+  `editor_id` int(11) DEFAULT NULL,
+  `creator_id` int(11) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `editor_id` (`editor_id`),
+  KEY `creator_id` (`creator_id`),
+  KEY `project_id` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(150) DEFAULT NULL,
+  `filename` varchar(150) DEFAULT NULL,
+  `realname` varchar(150) DEFAULT NULL,
+  `re` tinyint(1) DEFAULT NULL,
+  `top` tinyint(1) DEFAULT NULL,
+  `create_time` varchar(30) DEFAULT NULL,
+  `delete_time` varchar(30) DEFAULT NULL,
+  `creator_id` int(11) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `creator_id` (`creator_id`),
+  KEY `project_id` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+
+DROP TABLE IF EXISTS `user2files`;
+CREATE TABLE `user2files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `file_id` int(11) DEFAULT NULL,
+  `file_kind` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+
+-- 文件-文件夹
+DROP TABLE IF EXISTS `foldersforfiles`;
+CREATE TABLE `foldersforfiles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `create_time` varchar(30) DEFAULT NULL,
+  `create_id` int(11) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  `re` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `create_id` (`create_id`),
+  KEY `project_id` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+
+-- 文档-文件夹
+DROP TABLE IF EXISTS `foldersformds`;
+CREATE TABLE `foldersformds` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `create_time` varchar(30) DEFAULT NULL,
+  `create_id` int(11) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  `re` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `create_id` (`create_id`),
+  KEY `project_id` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
