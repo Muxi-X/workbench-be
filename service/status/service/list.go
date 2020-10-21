f760b502513b82535b6e261f01f35361dcbf28e6
JacksieCheung <1799902904@qq.com>
2020-10-21 21:19:27 +0800

    fix:fix status like problem

--- service/status/service/list.go
+++ service/status/service/list.go
@@ -24,31 +24,61 @@ func (s *StatusService) List(ctx context.Context, req *pb.ListRequest, res *pb.L
 
 	resList := make([]*pb.Status, 0)
 
-	for index := 0; index < len(list); index++ {
-		item := list[index]
-		resList = append(resList, &pb.Status{
-			Id:       item.ID,
-			Content:  item.Content,
-			Title:    item.Title,
-			Time:     item.Time,
-			Avatar:   item.Avatar,
-			UserName: item.UserName,
-		})
-		listLen := len(statusLikeList)
-		for j := 0; j < listLen; j++ {
-			if statusLikeList[j].StatusID == item.ID {
-				resList[index].Liked = 1
-				if j+1 < listLen {
-					statusLikeList = append(statusLikeList[:j], statusLikeList[j+1:]...)
-					listLen--
-					j--
-				} else {
-					statusLikeList = statusLikeList[:j]
-					listLen--
-					j--
-				}
-				break
-			}
+	// 合并结果
+	lenLike := len(statusLikeList)
+	lenList := len(list)
+	i := 0 // list 的指针
+	j := 0 // like 的指针
+	for i != lenList && j != lenLike {
+		if statusLikeList[j].StatusID > list[i].ID { // 如果 likelist 当前 id 比 statuslist id 大，说明不在范围内，直接跳过。
+			j++ // 只有 j 索引往后移动
+			continue
+		}
+		if statusLikeList[j].StatusID == list[i].ID { // 如果 likelist 当前 id 等于 statuslist id ，是该用户点赞的记录，liked 设置 1。
+			item := list[i]
+			resList = append(resList, &pb.Status{
+				Id:       item.ID,
+				Content:  item.Content,
+				Title:    item.Title,
+				Time:     item.Time,
+				Avatar:   item.Avatar,
+				UserName: item.UserName,
+				Liked:    uint32(1),
+			})
+			i++
+			j++
+			continue
+		}
+		if statusLikeList[j].StatusID < list[i].ID { // 如果 likelist 当前 id 小于 statuslist id，该记录不是目标记录，liked设置成 0。
+			item := list[i]
+			resList = append(resList, &pb.Status{
+				Id:       item.ID,
+				Content:  item.Content,
+				Title:    item.Title,
+				Time:     item.Time,
+				Avatar:   item.Avatar,
+				UserName: item.UserName,
+				Liked:    uint32(0),
+			})
+			i++ // 索引 i 往后走，j 等待目标
+			continue
+		}
+	}
+
+	// 若 statuslist 没走完，需要把剩下的status插入结果
+	if i < lenList {
+		for i != lenList {
+			item := list[i]
+			resList = append(resList, &pb.Status{
+				Id:       item.ID,
+				Content:  item.Content,
+				Title:    item.Title,
+				Time:     item.Time,
+				Avatar:   item.Avatar,
+				UserName: item.UserName,
+				Liked:    uint32(0),
+			})
+			i++
 		}
 	}
 
