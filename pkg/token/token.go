fbe3955c1c17e5b24519a3c56485452fcd9c9df9
Shadowmaple <1142319190@qq.com>
2020-11-11 17:00:01 +0800

    feat: modify the token claims

--- pkg/token/token.go
+++ pkg/token/token.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"time"
 
+	"muxi-workbench/log"
+
 	"github.com/dgrijalva/jwt-go"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
 var (
@@ -24,14 +27,6 @@ func getJwtKey() string {
 	return jwtKey
 }
 
-// TokenClaims means a claim segment in a JWT.
-type TokenClaims struct {
-	jwt.StandardClaims
-	ID     uint32 `json:"id"`
-	Role   uint32 `json:"role"`
-	TeamID uint32 `json:"team_id"`
-}
-
 // TokenPayload is a required payload when generates token.
 type TokenPayload struct {
 	ID      uint32        `json:"id"`
@@ -45,18 +40,15 @@ type TokenResolve struct {
 	ID     uint32 `json:"id"`
 	Role   uint32 `json:"role"`
 	TeamID uint32 `json:"team_id"`
-	// ExpiresAt int64  `json:"expires_at"` // 过期时间（时间戳，10位）
 }
 
 // GenerateToken generates token.
 func GenerateToken(payload *TokenPayload) (string, error) {
 	claims := &TokenClaims{
-		ID:     payload.ID,
-		Role:   payload.Role,
-		TeamID: payload.TeamID,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Unix() + int64(payload.Expired.Seconds()),
-		},
+		ID:        payload.ID,
+		Role:      payload.Role,
+		TeamID:    payload.TeamID,
+		ExpiresAt: time.Now().Unix() + int64(payload.Expired.Seconds()),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
@@ -65,7 +57,8 @@ func GenerateToken(payload *TokenPayload) (string, error) {
 
 // ResolveToken resolves token.
 func ResolveToken(tokenStr string) (*TokenResolve, error) {
-	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+	claims := &TokenClaims{}
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
@@ -73,48 +66,19 @@ func ResolveToken(tokenStr string) (*TokenResolve, error) {
 	})
 
 	if err != nil {
+		log.Error("Token parsing failed because of an internal error", zap.String("cause", err.Error()))
 		return nil, err
 	}
 
 	if !token.Valid {
+		log.Error("Token parsing failed; the token is invalid.")
 		return nil, ErrTokenInvalid
-	}
-
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if !ok {
-		return nil, ErrTokenInvalid
-	}
-
-	exp, ok := claims["exp"]
-	if !ok {
-		return nil, errors.New("The exp is not in token.")
-	}
-
-	// 校验有效时间
-	expiresAt := int64(exp.(float64))
-	if expiresAt <= time.Now().Unix() {
-		return nil, ErrTokenExpired
-	}
-
-	id, ok := claims["id"]
-	if !ok {
-		return nil, errors.New("The id is not in token.")
-	}
-
-	role, ok := claims["role"]
-	if !ok {
-		return nil, errors.New("The role is not in token.")
-	}
-
-	teamID, ok := claims["team_id"]
-	if !ok {
-		return nil, errors.New("The teamID is not in token.")
 	}
 
 	t := &TokenResolve{
-		ID:     uint32(id.(float64)),
-		Role:   uint32(role.(float64)),
-		TeamID: uint32(teamID.(float64)),
+		ID:     claims.ID,
+		Role:   claims.Role,
+		TeamID: claims.TeamID,
 	}
 	return t, nil
 }
