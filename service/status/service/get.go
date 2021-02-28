a0f221925eef08dbffafc6df3f5ec72267b6724c
JacksieCheung <1799902904@qq.com>
2021-02-28 17:36:19 +0800

    fix:status:modify get status detail response

--- service/status/service/get.go
+++ service/status/service/get.go
@@ -2,11 +2,14 @@ package service
 
 import (
 	"context"
+	"errors"
 
 	errno "muxi-workbench-status/errno"
 	"muxi-workbench-status/model"
 	pb "muxi-workbench-status/proto"
 	e "muxi-workbench/pkg/err"
+
+	"github.com/jinzhu/gorm"
 )
 
 // Get ... 获取动态
@@ -17,12 +20,25 @@ func (s *StatusService) Get(ctx context.Context, req *pb.GetRequest, res *pb.Get
 		return e.ServerErr(errno.ErrDatabase, err.Error())
 	}
 
+	// 获取 liked
+	liked := true
+	err = model.GetStatusLikeRecord(req.Uid, req.Id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			liked = false
+		} else {
+			return e.ServerErr(errno.ErrDatabase, err.Error())
+		}
+	}
+
 	res.Status = &pb.Status{
 		Id:      status.ID,
 		Title:   status.Title,
 		Content: status.Content,
 		UserId:  status.UserID,
 		Time:    status.Time,
+		Like:    status.Like,
+		Liked:   liked,
 	}
 
 	return nil
