f34734a16df3ece03bbb7ecba5eccb2c5f83c1e2
JacksieCheung <1799902904@qq.com>
2021-12-03 21:46:54 +0800

    test: print msg json

--- service/feed/service/push.go
+++ service/feed/service/push.go
@@ -21,8 +21,6 @@ func (s *FeedService) Push(ctx context.Context, req *pb.PushRequest, res *pb.Res
 		return e.ServerErr(errno.ErrGetDataFromRPC, err.Error())
 	}
 
-	fmt.Println("====>is here any username?",username)
-
 	feed := &model.FeedModel{
 		UserId:            req.UserId,
 		UserName:          username,
@@ -41,6 +39,7 @@ func (s *FeedService) Push(ctx context.Context, req *pb.PushRequest, res *pb.Res
 	if err != nil {
 		return e.ServerErr(errno.ErrJsonMarshal, err.Error())
 	}
+	fmt.Println("====>is here any username?",msg)
 
 	if err := model.PublishMsg(msg); err != nil {
 		logger.Error("Publish data error")
