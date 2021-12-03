b77abacf47e4e569dd66603d217e92c70199b39c
JacksieCheung <1799902904@qq.com>
2021-12-03 22:02:31 +0800

    test:print sub msg out

--- service/feed/service/subscribe.go
+++ service/feed/service/subscribe.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"muxi-workbench-feed/model"
 	logger "muxi-workbench/log"
@@ -15,6 +16,8 @@ func SubServiceRun() {
 	ch := m.PubSubClient.Self.Channel()
 	for msg := range ch {
 		logger.Info("received")
+		fmt.Println(msg.Payload)
+		fmt.Println(msg.String())
 
 		if err := json.Unmarshal([]byte(msg.Payload), feed); err != nil {
 			panic(err)
